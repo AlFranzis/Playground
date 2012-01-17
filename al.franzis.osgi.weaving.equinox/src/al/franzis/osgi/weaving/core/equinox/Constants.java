ad63a711d590d7462b8b12cc3e38d4285c2eb04f
al.franzis <al.franzis@gmail.com>
2012-01-17 23:16:21 +0100

    Changed package name

--- al.franzis.osgi.weaving.equinox/src/al/franzis/osgi/weaving/core/equinox/Constants.java
+++ al.franzis.osgi.weaving.equinox/src/al/franzis/osgi/weaving/core/equinox/Constants.java
@@ -2,7 +2,7 @@ package al.franzis.osgi.weaving.core.equinox;
 
 public class Constants {
 	
-	public static final String CORE_PACKAGE = "al.franzis.osgi.weaving.core";
+	public static final String CORE_PACKAGE = "al.franzis.osgi.weaving.core.equinox";
 	static final String METHODS_ARRAY_NAME = "amethods";
 	static final String HANDLER_FIELD_NAME = "handler";
 
