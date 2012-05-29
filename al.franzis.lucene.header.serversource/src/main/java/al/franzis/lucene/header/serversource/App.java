6ff07772e6e85c51aee106ea12c53d9e38d65bb2
al.franzis <al.franzis@gmail.com>
2012-05-29 22:34:34 +0200

    Initial import of serversource project

---
+++ al.franzis.lucene.header.serversource/src/main/java/al/franzis/lucene/header/serversource/App.java
@@ -0,0 +1,13 @@
+package al.franzis.lucene.header.serversource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ al.franzis.lucene.header.serversource/src/main/java/al/franzis/lucene/header/serversource/StudyUIDRetriever.java
@@ -0,0 +1,22 @@
+package al.franzis.lucene.header.serversource;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.dcm4che2.data.DicomObject;
+import org.dcm4che2.data.UID;
+import org.dcm4che2.data.UIDDictionary;
+import org.dcm4che2.net.Association;
+import org.dcm4che2.net.CommandUtils;
+import org.dcm4che2.net.DimseRSP;
+import org.dcm4che2.net.ExtQueryTransferCapability;
+import org.dcm4che2.net.NetworkApplicationEntity;
+import org.dcm4che2.net.NetworkConnection;
+import org.dcm4che2.net.NoPresentationContextException;
+import org.dcm4che2.net.TransferCapability;
+
+public class StudyUIDRetriever {
+	
+}
