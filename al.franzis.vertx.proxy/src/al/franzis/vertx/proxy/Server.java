15433e7a1e33218a8e1a06884d48099c1179939d
al.franzis <al.franzis@gmail.com>
2012-07-28 15:35:25 +0200

    Updated vertx proxy to start in embedded mode

--- al.franzis.vertx.proxy/src/al/franzis/vertx/proxy/Server.java
+++ al.franzis.vertx.proxy/src/al/franzis/vertx/proxy/Server.java
@@ -1,18 +1,34 @@
 package al.franzis.vertx.proxy;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.Vertx;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.logging.Logger;
-import org.vertx.java.deploy.Verticle;
+import org.vertx.java.core.logging.impl.LoggerFactory;
 
 
-public class Server extends Verticle {
-	private final Logger logger = container.getLogger();
+public class Server {
 	private final static String CRLF = "\r\n"; //$NON-NLS-1$
+	private static LoggerFactory loggerFactory; 
+	private final Logger logger = loggerFactory.getLogger(Server.class);
+	private final Vertx vertx = Vertx.newVertx();
+	
+	public static void main(String[] args) {
+//		Server.loggerFactory = new LoggerFactory();
+		
+		new Server().start();
+		try {
+			Thread.currentThread().join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public void start() {
+		
 		vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
 			public void handle(HttpServerRequest request) {
 		        logger.info("A request has arrived on the server!");
@@ -20,6 +36,7 @@ public class Server extends Verticle {
 		        
 		        
 		        String headerLine = request.uri;
+		        Map<String,String> paramsMap = request.params();
                 if ( headerLine == null || headerLine.equals( CRLF ) || headerLine.isEmpty() )
                     return;
 		        
